de1891f4fbb6cf18241fdec9f3ba6ed2a5ed06c6
Devani Ayush <ayushdevani0018@gmail.com>
2024-12-08 12:38:51 +0530

    aaa.

--- api.py
+++ api.py
@@ -152,12 +152,12 @@ hr {{
 <body>
        <!-- Add the header image at the start -->
     <div class="header-image">
-        <img src="stamp.jpg" alt="Header Image">
+        <img src="Images/stamp.jpg" alt="Header Image">
     </div>
 
     <div class="container">
         <div class="left-image">
-            <img src="left-image.png" alt="Left Image">
+            <img src="Images/left-image.png" alt="Left Image">
         </div>
         
         <div class="content">
@@ -171,7 +171,7 @@ hr {{
         </div>
         
         <div class="right-image">
-            <img src="right-image.png" alt="Right Image">
+            <img src="Images/right-image.png" alt="Right Image">
         </div>
     </div>
     
